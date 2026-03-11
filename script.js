1218f30ba3d706511db8e73216508cea6bf2fbd9
lamezinzi <zinzipontsho@gmail.com>
2026-03-11 11:40:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,28 @@ function typeEffect() {
  
 
 typeEffect();
+const backToTopButton = document.getElementById('back-to-top'); 
+
+ 
+
+window.addEventListener('scroll', function() { 
+
+    if (window.scrollY > 300) { 
+
+        backToTopButton.style.display = 'block'; 
+
+    } else { 
+
+        backToTopButton.style.display = 'none'; 
+
+    } 
+
+}); 
+
+ 
+
+backToTopButton.addEventListener('click', function() { 
+
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+
+}); 
