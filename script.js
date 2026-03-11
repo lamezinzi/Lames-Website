67c1caaf3e8d0839e0d5acc59dc1f210e9d63944
lamezinzi <zinzipontsho@gmail.com>
2026-03-11 11:28:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,72 @@ toggleButton.addEventListener('click', function() {
     } 
 
 }); 
+const texts = [ 
+
+    "Hi, I'm Your Name!", 
+
+    "I'm a Web Developer", 
+
+    "I Build Cool Things", 
+
+    "Welcome to My Portfolio!" 
+
+]; 
+
+ 
+
+let textIndex = 0; 
+
+let charIndex = 0; 
+
+let isDeleting = false; 
+
+const typingElement = document.getElementById('typing-headline'); 
+
+ 
+
+function typeEffect() { 
+
+    const currentText = texts[textIndex]; 
+
+    if (isDeleting) { 
+
+        typingElement.textContent = currentText.substring(0, charIndex - 1); 
+
+        charIndex--; 
+
+    } else { 
+
+        typingElement.textContent = currentText.substring(0, charIndex + 1); 
+
+        charIndex++; 
+
+    } 
+
+    if (!isDeleting && charIndex === currentText.length) { 
+
+        isDeleting = true; 
+
+        setTimeout(typeEffect, 2000); 
+
+        return; 
+
+    } 
+
+    if (isDeleting && charIndex === 0) { 
+
+        isDeleting = false; 
+
+        textIndex = (textIndex + 1) % texts.length; 
+
+    } 
+
+    const speed = isDeleting ? 50 : 100; 
+
+    setTimeout(typeEffect, speed); 
+
+} 
+
+ 
+
+typeEffect();
